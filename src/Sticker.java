66211fcf01ce2ec4ccda16237fcea196b00f6881
synature14 <synature14@gmail.com>
2017-10-03 23:02:05 +0900

    dynamic programming

--- src/Sticker.java
+++ src/Sticker.java
@@ -1,7 +1,47 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+
 
 /**
- * Created by suyeon on 2017-10-03.
+ * 스티커
+ * https://www.acmicpc.net/problem/9465
  */
 public class Sticker {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[][] sticker = new int[3][n+1];       //행(1행부터 시작함),열
+
+        for(int i=1; i<3; i++){
+            for(int j=1; j<=n; j++){
+                sticker[i][j] = scan.nextInt();
+            }
+        } //for() 입력
+
+        int[][] dp = new int[n+1][3]; // 2*n개의 스티커중 점수합 최대, n열의 상태는 총 3가지 (0,0), (0,1), (1,0)
+
+        dp[1][0] = 0;       //sticker 어느것도 떼지 않은 상태
+        dp[1][1] = sticker[1][1];   //sticker 첫번째 열의 1행과 2행 중 위(1행)에 위치한 스티커 떼었을경우
+        dp[1][2] = sticker[2][1];   //sticker 첫번째 열의 아래(2행)에 위치한 스티커 떼었을경우
+
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<3; j++){
+                if(j == 0){    // sticker배열의 i열의 위/아래를 떼거나 or 모두 떼지 않는 경우
+                    dp[i][j] = max(  max(dp[i-1][0], dp[i-1][1]),   dp[i-1][2] );
+                }
+                if(j == 1){    // sticker배열의 i열의 위를 떼는 경우
+                    dp[i][j] = max(dp[i-1][0] + sticker[1][i], dp[i-1][2] + sticker[1][i]); //sticker 첫번째 행의 i번째 열 값
+                }
+                if(j == 2) {    //sticker배열의 i열의 아래를 떼는 경우
+                    dp[i][j] = max(dp[i - 1][0] + sticker[2][i], dp[i - 1][1] + sticker[2][i]); //sticker 두번째 행의 i번째 열 값
+                }
+            }
+        } //for()
+
+        System.out.println(" 정답 : " + max( max(dp[n][0], dp[n][1]) , dp[n][2]) );
+
+    }
 }
--- src/TastingWine.java
+++ src/TastingWine.java
@@ -1,7 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import static java.lang.Math.max;
+
 
 /**
- * Created by suyeon on 2017-10-03.
+ * 포도주 시식하기
+ * https://www.acmicpc.net/problem/2156
  */
 public class TastingWine {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] wine = new int[n+1];
+
+        for(int i=1; i<=n; i++){
+            wine[i] = scan.nextInt();
+        } //입력
+
+        int[][] dp = new int[n+1][3];   // 1~n번째까지 포도주 마신양 중 연속으로 0/1/2번 마셨을 경우
+        dp[1][0] = 0;       //0번 마셨다
+        dp[1][1] = wine[1];     //  1번 마셨다
+        dp[1][2] = 0;     //  2번 연속으로 마셨다 --> 1번째까지 포도주 마실수 있는데 두번 마실수가 없음
+
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<=2; j++){    // 0번, 1번, 2번 마시는 경우
+                if(j == 0){         //n번째에 마시지 않았다면, n-1번째에는 0번 or 1번 or 2번 마시는 경우
+                    dp[i][j] = max(  max(dp[i-1][0] , dp[i-1][1])  ,  dp[i-1][2] );
+                }
+                if(j == 1){         // n번째에 1번 연속으로 마셨다 --> 무조건 n-1번째에는 안마시고 + wine[n]
+                    dp[i][j] = dp[i-1][0] + wine[i];
+                }
+                if(j == 2){         // n번째까지 총 연속으로 두번 마신다 --> 무조건 n-1번째에는 마셔야 한다! + wine[n]
+                    dp[i][j] = dp[i-1][1] + wine[i];
+                }
+            }
+        } // for()
+
+        System.out.println(" 정답 : " + max(  max(dp[n][0], dp[n][1]),   dp[n][2] )  );
+
+    }
 }
